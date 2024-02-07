b535275db71738a495943a618f4b097fedf12eb7
Kemy10 <120021911+Kemy10@users.noreply.github.com>
2024-02-07 09:12:20 -0300

    Add files via upload

--- php-iniciante-crud-level-1/index.php
+++ php-iniciante-crud-level-1/index.php
@@ -1,16 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Texto</title>
+    <title>PDO - Ler Registros - Tutorial de PHP CRUD </title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <!-- Css customizado -->
+    <style>
+        .m-r-1em {
+            margin-right: 1em;
+        }
+
+        .m-b-1em {
+            margin-bottom: 1em;
+        }
+
+        .m-l-1em {
+            margin-left: 1em;
+        }
+
+        .mt0 {
+            margin-top: 0;
+        }
+    </style>
+
+
 </head>
-<body>
-    
-
-
-
-
-
-</body>
-</html>
